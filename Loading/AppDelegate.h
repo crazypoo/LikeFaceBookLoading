e960acea2bfc580aa74b6d1635a47719ee76c4c1
crazypoo <273277355@qq.com>
2014-01-07 10:09:58 +0800

    Initial Commit

---
+++ Loading/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Loading
+//
+//  Created by crazypoo on 1/7/14.
+//  Copyright (c) 2014 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Loading/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Loading
+//
+//  Created by crazypoo on 1/7/14.
+//  Copyright (c) 2014 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
